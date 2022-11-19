9f21b728d27ce34d2a765dfb74a383a85561608a
김기윤 <crakel528@ajou.ac.kr>
2022-11-19 15:20:24 +0900

    fix : CORS config5

--- auction/src/main/java/com/the_ajou/config/WebConfig.java
+++ auction/src/main/java/com/the_ajou/config/WebConfig.java
@@ -11,7 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .exposedHeaders("X-AUTH-TOKEN")
-                .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     }
 }
