d609cce3d1c060775b6163334f56f6d58d7c794c
김현빈 <kimhb09@ajou.ac.kr>
2022-11-28 11:04:29 +0900

    swagger 주소 routing

--- auction/src/main/java/com/the_ajou/AuctionApplication.java
+++ auction/src/main/java/com/the_ajou/AuctionApplication.java
@@ -2,9 +2,16 @@ package com.the_ajou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 
 import javax.annotation.PostConstruct;
+import java.net.URI;
 import java.util.TimeZone;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class AuctionApplication {
@@ -12,6 +19,11 @@ public class AuctionApplication {
 	@PostConstruct
 	public void setTimeZone(){
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return route(GET("/swagger"), req ->
+				ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
 	}
 
 	public static void main(String[] args) {
