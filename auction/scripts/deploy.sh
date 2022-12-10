e849a143cd8fc73ba1956aa6cf6cf264ee13f607
GY KIM <59333136+crakel@users.noreply.github.com>
2022-12-10 19:34:33 +0900

    test : deploy nvm fix

--- auction/scripts/deploy.sh
+++ auction/scripts/deploy.sh
@@ -22,6 +22,15 @@ fi
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 chmod +x $DEPLOY_JAR
 
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+# This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. 
+"$NVM_DIR/bash_completion"  
+# This loads nvm bash_completion
+
+nvm use 16
+
 echo "> pm2 배포"    >> /home/ubuntu/api-server/deploy.log
 pm2 start /home/ubuntu/api-server/app.json >> /home/ubuntu/deploy.log 2>/home/ubuntu/api-server/deploy_err.log &
 
